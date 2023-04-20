67ae30e4056567275a43dbb278064020c9b9fcdb
caryg <caryg@PHAWXTOP>
2023-04-19 22:47:02 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.dgvDuplicates = new System.Windows.Forms.DataGridView();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDeselectAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDuplicates)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // dgvDuplicates
+            // 
+            this.dgvDuplicates.AllowUserToAddRows = false;
+            this.dgvDuplicates.AllowUserToOrderColumns = true;
+            this.dgvDuplicates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDuplicates.Location = new System.Drawing.Point(12, 41);
+            this.dgvDuplicates.Name = "dgvDuplicates";
+            this.dgvDuplicates.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDuplicates.Size = new System.Drawing.Size(776, 336);
+            this.dgvDuplicates.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 399);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(776, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "DELETE FILES";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDeselectAll
+            // 
+            this.btnDeselectAll.Location = new System.Drawing.Point(93, 12);
+            this.btnDeselectAll.Name = "btnDeselectAll";
+            this.btnDeselectAll.Size = new System.Drawing.Size(75, 23);
+            this.btnDeselectAll.TabIndex = 4;
+            this.btnDeselectAll.Text = "Deselect All";
+            this.btnDeselectAll.UseVisualStyleBackColor = true;
+            this.btnDeselectAll.Click += new System.EventHandler(this.btnDeselectAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDeselectAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dgvDuplicates);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDuplicates)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.DataGridView dgvDuplicates;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDeselectAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog.SelectedPath;
+                FindDuplicateFiles(folderPath);
+            }
+        }
+
+        private void FindDuplicateFiles(string folderPath)
+        {
+            Dictionary<string, List<(string filePath, Version version)>> fileMap = new Dictionary<string, List<(string filePath, Version version)>>();
+
+            foreach (var filePath in Directory.GetFiles(folderPath, "*.nsp"))
+            {
+                string fileName = Path.GetFileName(filePath);
+                string fileKey = Regex.Replace(fileName, @"(\[UPD\]|\bUpd\b)?\s*\[?v?\d+\.\d+\.\d+\]?", string.Empty, RegexOptions.IgnoreCase).Trim();
+                Match versionMatch = Regex.Match(fileName, @"\d+\.\d+\.\d+");
+
+                if (versionMatch.Success && Version.TryParse(versionMatch.Value, out Version version))
+                {
+                    if (!fileMap.ContainsKey(fileKey))
+                    {
+                        fileMap[fileKey] = new List<(string filePath, Version version)>();
+                    }
+
+                    fileMap[fileKey].Add((filePath, version));
+                }
+            }
+
+            dgvDuplicates.Rows.Clear();
+            dgvDuplicates.Columns.Clear();
+
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            checkBoxColumn.Name = "Select";
+            checkBoxColumn.HeaderText = "Select";
+            checkBoxColumn.ReadOnly = false;
+            dgvDuplicates.Columns.Add(checkBoxColumn);
+
+            dgvDuplicates.Columns.Add("FileName", "File Name");
+            dgvDuplicates.Columns.Add("FilePath", "File Path");
+
+            foreach (var entry in fileMap)
+            {
+                if (entry.Value.Count > 1)
+                {
+                    Version maxVersion = entry.Value.Max(x => x.version);
+                    foreach (var (filePath, version) in entry.Value)
+                    {
+                        bool isOldVersion = version.CompareTo(maxVersion) < 0;
+                        int rowIndex = dgvDuplicates.Rows.Add(isOldVersion, Path.GetFileName(filePath), filePath);
+                        dgvDuplicates.Rows[rowIndex].Cells["Select"].Value = isOldVersion;
+                    }
+                }
+            }
+
+            dgvDuplicates.ReadOnly = true;
+            dgvDuplicates.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure you want to delete the selected files?", "Delete Confirmation",
+        MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                for (int i = dgvDuplicates.Rows.Count - 1; i >= 0; i--)
+                {
+                    DataGridViewRow row = dgvDuplicates.Rows[i];
+                    bool isSelected = Convert.ToBoolean(row.Cells["Select"].Value);
+
+                    if (isSelected)
+                    {
+                        string filePath = row.Cells["FilePath"].Value.ToString();
+                        try
+                        {
+                            File.Delete(filePath);
+                            dgvDuplicates.Rows.RemoveAt(i);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error deleting file: " + filePath + "\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnDeselectAll_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgvDuplicates.Rows)
+            {
+                row.Cells["Select"].Value = false;
+            }
+        }
+    }
+}
